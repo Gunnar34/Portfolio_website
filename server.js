36e44f31bfd3ad0d4755c51f27fe7b87970200b9
Noah Rolf <noahrolf1@gmail.com>
2017-08-14 09:21:25 -0500

    heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const nodemailer = require('nodemailer');
 const path = require('path');
 const express = require('express');
-const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config.js');
+// const webpack = require('webpack');
+// const webpackMiddleware = require('webpack-dev-middleware');
+// const webpackHotMiddleware = require('webpack-hot-middleware');
+// const config = require('./webpack.config.js');
 // const authConfig = require('./config.js');
 const bodyParser = require('body-parser');
 const aws = require('aws-sdk');
@@ -26,35 +26,35 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-if (isDeveloping) {
-  const compiler = webpack(config);
-  const middleware = webpackMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-    contentBase: 'src',
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false
-    }
-  });
-
+// if (isDeveloping) {
+//   const compiler = webpack(config);
+//   const middleware = webpackMiddleware(compiler, {
+//     publicPath: config.output.publicPath,
+//     contentBase: 'src',
+//     stats: {
+//       colors: true,
+//       hash: false,
+//       timings: true,
+//       chunks: false,
+//       chunkModules: false,
+//       modules: false
+//     }
+//   });
+//
+//   app.use(middleware);
+//   app.use(webpackHotMiddleware(compiler));
+//   app.get('*', function response(req, res) {
+//     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+//     res.end();
+//   });
+// } else {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
-  app.get('*', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-    res.end();
-  });
-} else {
   app.use(express.static(__dirname + '/dist'));
   app.get('*', function response(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
-}
+// }
 
 app.post('/email', function prep(req, res) {
   const mailOptions = {
